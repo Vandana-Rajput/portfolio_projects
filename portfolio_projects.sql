e0f7684fcd334aa6700fddca5dcba319636a32e1
Vandana-Rajput <90293303+Vandana-Rajput@users.noreply.github.com>
2021-09-08 16:23:17 +0530

    Update portfolio_projects.sql

--- portfolio_projects.sql
+++ portfolio_projects.sql
@@ -1,103 +1,6 @@
 use portfolio_project;
-create table coviddeaths(
-iso_code varchar(10),
-continent varchar(40),
-location varchar(40),
-date date,
-population bigint,
-total_cases bigint,
-new_cases int,
-new_cases_smoothed float(11,3),
-total_deaths int,
-new_deaths  int,
-new_deaths_smoothed float(10,3),
-total_cases_per_million float(10,3),
-new_cases_per_million float(8,3),
-new_cases_smoothed_per_million float(9,3),
-total_deaths_per_million float(10,3),
-new_deaths_per_million float(8,3),
-new_deaths_smoothed_per_million float(8,3),
-reproduction_rate float(5,2),
-icu_patients bigint,
-icu_patients_per_million float(10,3),
-hosp_patients bigint,
-hosp_patients_per_million float(7,3),
-weekly_icu_admissions float(10,3),
-weekly_icu_admissions_per_million float(10,3),
-weekly_hosp_admissions float(10,3),
-weekly_hosp_admissions_per_million double);
 
-
-
-
-load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/coviddeaths.csv' into table coviddeaths FIELDS terminated by ','  enclosed by '"' lines terminated by '\n' ignore 1 rows;
-
-load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads/covidvaccinations.csv' into table
-covidvaccinations FIELDS terminated by ','  enclosed by '"' lines terminated by '\r\n' 
-ignore 1 lines SET date = DATE_FORMAT(date,'%Y-%m-%d');
-
-select *  from coviddeaths;
-select date, location, total_cases, total_deaths, total_deaths*100/total_cases as death_rate  
-from coviddeaths where location = "India";
-
--- Total Cases vs population
-select date, location, population, total_cases, total_cases*100/population as infected_pct  
-from coviddeaths;
-
-select location, population, max(total_cases), max(total_cases)*100/population as infected_pct  
-from coviddeaths group by 1 order by 4 desc;
-
-select location, population, max(total_deaths), max(total_deaths)*100/max(total_cases) as death_rate  
-from coviddeaths group by 1 order by 4 desc;
-
-select location, max(total_deaths)
-from coviddeaths where location <> continent 
-group by 1 order by 2 desc;
-
-
-
-select continent, location , max(total_deaths)
-from coviddeaths  where location <> continent
-group by 1,2 order by 1;
-   
-select continent , max(total_deaths)
-from coviddeaths where location <> continent group by 1 order by 2 desc;
-   
-select continent , max(total_deaths)
-from coviddeaths where location = continent group by 1 order by 2 desc;
-
--- global numbers
-
-select date, sum(new_cases), sum(new_deaths),  sum(new_deaths)*100/sum(new_cases) death_rate
-from coviddeaths 
-group by 1 order by 1;
-
-select * from coviddeaths dea join covidvaccinations vac on vac.location = dea.location and 
-vac.date= dea.date;
- 
-select * from coviddeaths;
-select * from covidvaccinations;
--- Total population vs vaccination
-
-select continent, location,  max(people_fully_vaccinated) 
-from covidvaccinations  group by 1, 2 order by 1,2;
-
-select continent,  population, max(people_fully_vaccinated) 
-from covidvaccinations  group by 1 order by 2 desc;
--- Total population vs vaccination
-create temporary table f;
-select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-sum(vac.new_vaccinations) over (partition by dea.location order by dea.date ) rolling_no
-from covidvaccinations vac join coviddeaths dea on dea.location = vac.location and dea.date = vac.date
-where dea.continent <>  dea.location group by 1,2, 3 order by 1,2,3 ;
-
-select f.*, max(rolling_no)*100/population as pct_people_vaccinated from f group by 1,2,3 
-order by 2,3;
-
-
-
-
--- Link to dataset ... https://ourworldindata.org/covid-deaths
+-- Link to dataset https://ourworldindata.org/covid-deaths
 -- Importing CSV file to MySQL workbench
 # - /*load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads/covidvaccinations.csv' into table
 # -  covidvaccinations FIELDS terminated by ','  enclosed by '"' lines terminated by '\r\n' 
@@ -109,6 +12,7 @@ order by 2,3;
 
 
 -- Total cases vs Total death(likelihood of dying if you catch corona virus)
+
 SELECT 
     location,
     date,
